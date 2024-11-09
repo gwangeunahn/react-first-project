87dd100e8b085e146bdec21c16b52844f0155df7
gwangeunahn <gwangeunahn@handong.ac.kr>
2024-11-09 21:16:14 +0900

    #002

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
+import Info from './components/Info';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Hello/>
+        <Info/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Name : gwangeunahn
+          <br/>
+          Student ID : 22100427
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/components/Info.js
@@ -0,0 +1,9 @@
+import './Info.css';
+
+function Info(props){
+    return (
+        <h3>Student Information</h3>
+    );
+}
+
+export default Info;
